5e93b7965f332bae56787b99931862bb67d3a515
jayweston <teacum@gmail.com>
2015-03-11 11:16:16 -0600

    stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 	$( ".modal_home-wrapper" ).hide();
-	$(".modal_home-wrapper").html('<form action="home.html"><input type="submit" value="Home" class="test-button btn"></form><form action="groups.html"><input type="submit" value="Groups" class="test-button btn"></form><form action="create_lesson1.html"><input type="submit" value="Create Lesson" class="test-button btn"></form><form action="find_lesson.html"><input type="submit" value="Find Lesson" class="test-button btn"></form><form action="settings.html"><input type="submit" value="Settings" class="test-button btn"></form><form action="../index.html"><input type="submit" value="Sign Out" class="test-button btn"></form>'); 
+	$(".modal_home-wrapper").html('<form action="home.html"><input type="submit" value="Home" class="test-button btn"></form><form action="inbox.html"><input type="submit" value="Inbox" class="test-button btn"></form><form action="groups.html"><input type="submit" value="Groups" class="test-button btn"></form><form action="create_lesson1.html"><input type="submit" value="Create Lesson" class="test-button btn"></form><form action="find_lesson.html"><input type="submit" value="Find Lesson" class="test-button btn"></form><form action="settings.html"><input type="submit" value="Settings" class="test-button btn"></form><form action="../index.html"><input type="submit" value="Sign Out" class="test-button btn"></form>'); 
 });
 
 function show_menu(){
